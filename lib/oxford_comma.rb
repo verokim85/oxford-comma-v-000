07f54a488f48c90bab72c6f7250816e37b6b5f27
Veronica Kim <vero.a.kim@gmail.com>
2018-01-23 01:34:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  x = []
+  y = []
 
+  if array.length == 1
+    array.join(",")
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+  p  x <<  array.pop
+     y <<  array[0...-1]
+     z = array.join(", ")
+     final = z + ", " + "and" + " "+ x.join
+  end
 end
